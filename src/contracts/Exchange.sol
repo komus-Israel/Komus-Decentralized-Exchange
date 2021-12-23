9104473aa4a5c302b18fd5bd7325a9c4399d6dd4
komus-Israel <komolehinIsrael@gmail.com>
2021-12-23 08:36:22 +0100

    created the Withdraw event to be emitted when ether or any token is withdrawn

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -30,6 +30,10 @@ contract Exchange {
     // emit a deposit event
     event Deposit(address _token, address _user, uint256 _amount, uint256 _balance);
 
+
+    // emit withdraw event
+    event Withdraw(address _ether, address _user, uint256 _amount, uint256 _balance);
+
     constructor (address _transactionFeeAccount, uint256 _transactionFeePercent) {
 
         transactionFeeAccount = _transactionFeeAccount;
@@ -75,6 +79,7 @@ contract Exchange {
         require(_amount <= tokens[ETHER][msg.sender]);
         // reduce the value to be withdrawn from the ether tokens map
         tokens[ETHER][msg.sender] -= _amount;
+        
     }
 
     
