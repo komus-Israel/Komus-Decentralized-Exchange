1e1dd872608e24eb345ae58dbf0d0a78f297590b
komus-Israel <komolehinIsrael@gmail.com>
2021-12-27 20:08:43 +0100

    corrected method to getAccounts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const Exchange = artifacts.require("Exchange")
 
 module.exports = async function (deployer) {
 
-  const accounts = await web3.eth.getAccount()
+  const accounts = await web3.eth.getAccounts()
 
   const feeAccount = accounts[0]
   const feePercent = 10;
