b5dc11e9f3cf2e63dc25cbb4b23c695181eb7457
komus-Israel <komolehinIsrael@gmail.com>
2021-12-27 20:06:02 +0100

    assigned feeAccount and feePercent to a variable

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,9 +4,12 @@ const Exchange = artifacts.require("Exchange")
 module.exports = async function (deployer) {
 
   const accounts = await web3.eth.getAccount()
-  
+
+  const feeAccount = accounts[0]
+  const feePercent = 10;
+
   await deployer.deploy(Token, "Komus", "KOM");
-  await deployer.deploy(Exchange, feeAccount ,10)
+  await deployer.deploy(Exchange, feeAccount ,feePercent)
 };
 
 
