88c471259187835f1ef61310f1fe0b9a9a125782
komus-Israel <komolehinIsrael@gmail.com>
2021-12-27 20:32:50 +0100

    create function to send tokens

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -4,12 +4,15 @@ const Token = artifacts.require('Token')
 const Exchange = artifacts.require('Exchange')
 
 
+
+
 module.exports = async function() {
 
     try {
 
         // get the accounts
         const accounts = await web3.eth.getAccounts()
+        const deployer = accounts[0]
 
         //  fetch the deployed Token Contract
         const token = await Token.deployed()
@@ -24,6 +27,15 @@ module.exports = async function() {
         // check the token balance of the first account
         const tokenBalance = await token.balanceOf(accounts[0])
         console.log('token balance of the first account', tokenBalance.toString())
+
+
+        // create function to send tokens
+        async function sendTokens(recipient){
+            await token.transfer(recipient,  { from: deployer })
+        }
+
+        // transfer some tokens to two other accounts
+        sendTokens(accounts[1])
 
     } catch (err) {
         console.log(err)
