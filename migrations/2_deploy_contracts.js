5e172599a2161946f925f297bba63a6659fb63e3
komus-Israel <komolehinIsrael@gmail.com>
2021-12-27 20:04:07 +0100

    initialized accounts using web3

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,9 @@ const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange")
 
 module.exports = async function (deployer) {
+
+  const accounts = await web3.eth.getAccount()
+  
   await deployer.deploy(Token, "Komus", "KOM");
   await deployer.deploy(Exchange, feeAccount ,10)
 };
