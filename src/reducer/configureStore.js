54894a7bfcd25f4077c6da05993dd27d71d408fe
komus-Israel <komolehinIsrael@gmail.com>
2021-12-30 20:58:16 +0100

    configured the middle ware

--- src/reducer/configureStore.js
+++ src/reducer/configureStore.js
@@ -4,12 +4,15 @@ import { createStore } from "redux";
 
 
 const loggerMiddleWare = createLogger()
+const middleWare = []
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose
 
 
 export const configureStore=(preloadedState)=>{
     return createStore(
 
         combinedReducers,
-        preloadedState
+        preloadedState,
+        composeEnhancer(applyMiddleWare(...middleWare, loggerMiddleWare))
     )
 }
