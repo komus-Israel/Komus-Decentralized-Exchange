79a12ce8d138a758ff4fb47b2f6d388728ce4537
komus-Israel <komolehinIsrael@gmail.com>
2022-01-03 20:55:28 +0100

    using lodash get function with useSelectors

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,28 @@
 import { increment } from "../actions"
 import { useDispatch, useSelector } from "react-redux"
+import { get } from "lodash"
 
 
 
 const Header=()=>{
 
     const counts = useSelector(
-        state => state.counter
+        state => get(state, 'counter')
     )
 
+
     const dispatch = useDispatch()
+
+    console.log(counts)
+
+    
     
 
     return (
         <nav>
             <p>This is the header</p>
             <p>{counts}</p>
+        
             <button onClick={()=>dispatch(increment())}>increment</button>
         </nav>
     )
