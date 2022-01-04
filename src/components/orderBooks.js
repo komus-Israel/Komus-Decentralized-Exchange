e0cf7bf3dfa6ba4a02e6c0040faf28587f48ffff
komus-Israel <komolehinIsrael@gmail.com>
2022-01-04 22:20:43 +0100

    set the height of the order book and trades component

--- src/components/orderBooks.js
+++ src/components/orderBooks.js
@@ -2,7 +2,23 @@ const OrderBook=()=>{
     return(
         <div className="order-book">
             <p className="cont-header">Order Book</p>
-            <p>The component for the order book</p>
+            <div className="dex-content">
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p><p>The component for the order book</p>
+                <p>The component for the order book</p>
+                <p>The component for the order book</p>
+            </div>
+            
         </div>
     )
 }
