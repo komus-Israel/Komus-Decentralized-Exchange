ee8a64c1d3c3112a3cfc9bda61003849f548bcc1
komus-Israel <komolehinIsrael@gmail.com>
2022-01-04 19:40:59 +0100

    imported the components

--- src/components/body.js
+++ src/components/body.js
@@ -1,8 +1,14 @@
 import '../styles/body.css';
+import OrderBook from './orderBooks';
+import Chart from './chart';
+import Transactions from './transactions';
+import Trades from './trades';
+import Balance from './balance';
+import NewOrder from './newOrder';
 
 const Body=()=>{
     return (
-        <div>
+        <div className='main-body'>
            
         </div>
     )
