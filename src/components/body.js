47c1d9064179397308ccf158db52cd233303c9c2
komus-Israel <komolehinIsrael@gmail.com>
2022-01-04 19:35:02 +0100

    created file for all the necessary components

--- src/components/body.js
+++ src/components/body.js
@@ -1,12 +1,9 @@
-import LowerBody from "./lowerBody";
-import UpperBody from "./upperBody";
 import '../styles/body.css';
 
 const Body=()=>{
     return (
         <div>
-            <UpperBody />
-            <LowerBody />
+           
         </div>
     )
 }
