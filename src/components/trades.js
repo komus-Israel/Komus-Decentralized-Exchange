a8e44cef11d4addcaa004d309b9e7470f9be1c2a
komus-Israel <komolehinIsrael@gmail.com>
2022-01-05 12:33:42 +0100

    fetch the filled orders from redux into the trade component

--- src/components/trades.js
+++ src/components/trades.js
@@ -1,9 +1,14 @@
 import { useSelector } from "react-redux"
-
-
+import { get } from "lodash"
 
 
 const Trades=()=>{
+
+    const trades = useSelector(
+        state => get(state, 'loadEventsReducer.filledOrders')
+    )
+
+    
     return(
         <div className="trades">
              <p className="cont-header">Trades</p>
