1a88c389c69fa62a7826900ee231206f64f498b0
komus-Israel <komolehinIsrael@gmail.com>
2022-01-08 18:20:26 +0100

    created component for the user open orders and filled orders

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -6,38 +6,75 @@ const Transactions=()=>{
     const [myOpenOrder, setOpenOrder] = useState('')
     const [myFilledOrder, setFilledOrder] = useState('')
 
-    const order = useRef()
-    const filled = useRef()
+    const openOrder = useRef()
+    const trades = useRef()
     return(
         <div className="transactions">
              <p className="cont-header">Transactions</p>
              <div className="dex-content">
 
-                 <button onClick={()=>{
+                 <table>
+                     <tbody>
+                         <tr>
+                             <th onClick={()=>{
+                                 openOrder.current.hidden = false 
+                                 trades.current.hidden = true
+                             }}>Open Orders</th>
+                             <th>Filled Orders</th>
+                         </tr>
+                     </tbody>
+
+                     <tbody ref = {openOrder}>
+                        <MyOpenOrders />
+                     </tbody>
+
+                     <tbody ref = {trades}> 
+                        <MyTrades />
+                     </tbody>
+
+                 </table>
+
+                 {/*<button onClick={()=>{
                      filled.current.hidden = true
                      order.current.hidden = false
                      setOpenOrder('these are my open orders')
                  }}>my open orders</button>
-                 
+
                  <button onClick={()=>{
                      order.current.hidden = true
                      filled.current.hidden = false
                      setFilledOrder('these are my filled orders')
-                 }}>my transactions</button>
+                 }}>my transactions</button>*/}
 
 
-                 <p ref={order} onClick={()=>console.log(order)}>
-                     {myOpenOrder}
-                 </p>
-
-
-                 <p ref={filled} onClick={()=>console.log(order)}>
-                     {myFilledOrder}
-                 </p>
                 
             </div>
         </div>
     )
 }
+
+const MyOpenOrders=()=>{
+    return (
+        <table>
+            <th>Amount</th>
+            <th>KOM/ETH</th>
+            <th>ETH</th>
+        </table>
+    )
+}
+
+
+
+const MyTrades=()=>{
+    return (
+        <table>
+            <th>Amount</th>
+            <th>My trades</th>
+        </table>
+    )
+}
+
+
+
 
 export default Transactions
