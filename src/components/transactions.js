013b851e254b2e0af60c8b2c3732d3341f8a19e7
komus-Israel <komolehinIsrael@gmail.com>
2022-01-08 18:08:44 +0100

    used useRef to control the visibility of elements

--- src/components/transactions.js
+++ src/components/transactions.js
@@ -1,8 +1,39 @@
+import { useRef } from "react"
+import { useState } from "react"
+
 const Transactions=()=>{
+
+    const [myOpenOrder, setOpenOrder] = useState('')
+    const [myFilledOrder, setFilledOrder] = useState('')
+
+    const order = useRef()
+    const filled = useRef()
     return(
         <div className="transactions">
              <p className="cont-header">Transactions</p>
              <div className="dex-content">
+
+                 <button onClick={()=>{
+                     filled.current.hidden = true
+                     order.current.hidden = false
+                     setOpenOrder('these are my open orders')
+                 }}>my open orders</button>
+                 
+                 <button onClick={()=>{
+                     order.current.hidden = true
+                     filled.current.hidden = false
+                     setFilledOrder('these are my filled orders')
+                 }}>my transactions</button>
+
+
+                 <p ref={order} onClick={()=>console.log(order)}>
+                     {myOpenOrder}
+                 </p>
+
+
+                 <p ref={filled} onClick={()=>console.log(order)}>
+                     {myFilledOrder}
+                 </p>
                 
             </div>
         </div>
