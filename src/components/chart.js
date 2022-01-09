8534abe57cfb144d3cf88d501568a23e2b201b6c
komus-Israel <komolehinIsrael@gmail.com>
2022-01-09 20:26:03 +0100

    declared the type of the chart as candlestick

--- src/components/chart.js
+++ src/components/chart.js
@@ -6,7 +6,8 @@ const PriceChart=()=>{
     return(
         <div className="chart">
              <p className="cont-header">Price Chart</p>
-            <p>The component for the price chart</p>
+            
+            <Chart type="candlestick"/>
         </div>
     )
 }
