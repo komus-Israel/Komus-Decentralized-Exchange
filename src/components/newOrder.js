678c962bc2cfae658c2a21902a29c70ac3a4d334
komus-Israel <komolehinIsrael@gmail.com>
2022-01-16 14:35:18 +0100

    created ref for the buy and sell button

--- src/components/newOrder.js
+++ src/components/newOrder.js
@@ -1,12 +1,19 @@
+import { useRef } from "react"
 
 
 
 const NewOrder=()=>{
+
+    const buy = useRef()
+    const sell = useRef()
+
     return(
         <div className="new-order">
              <p className="cont-header">New Order</p>
              <div className="dex-content">
 
+                <button>Buy</button>
+                <button>Sell</button>
             
                 
             </div>
